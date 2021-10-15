e647984e352f83e04cdf8fca1b088f49ddd21071
ars <ars.anosov@gmail.com>
2021-10-15 18:44:04 +0300

    Стартовал свой контейнер

---
+++ my_scripts/client_connect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function mail_input() {
+  echo "To: $1"
+  echo "From: ars-spamer@yandex.ru"
+  echo "Subject: ovpn-up home"
+  echo ""
+  echo "UP $common_name OVPN $ifconfig_local REMOTE $ifconfig_pool_remote_ip FROM $untrusted_ip"
+}
+
+# /sbin/arp -i eth0 -Ds $ifconfig_pool_remote_ip eth0 pub
+
+#mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
---
+++ my_scripts/client_disconnect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function mail_input() {
+  echo "To: $1"
+  echo "From: ars-spamer@yandex.ru"
+  echo "Subject: ovpn-down home"
+  echo ""
+  echo "DOWN $common_name OVPN $ifconfig_local REMOTE $ifconfig_pool_remote_ip FROM $untrusted_ip"
+}
+
+# /sbin/arp -i eth0 -d $ifconfig_pool_remote_ip
+
+#mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
