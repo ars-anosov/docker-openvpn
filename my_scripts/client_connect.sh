0f606af97c791563a4d93f465a8cdec36606a359
ars <ars.anosov@gmail.com>
2021-10-16 09:21:10 +0300

    e-mail alerts

--- my_scripts/client_connect.sh
+++ my_scripts/client_connect.sh
@@ -3,11 +3,11 @@
 function mail_input() {
   echo "To: $1"
   echo "From: ars-spamer@yandex.ru"
-  echo "Subject: ovpn-up home"
+  echo "Subject: ovpn-up VPS"
   echo ""
   echo "UP $common_name OVPN $ifconfig_local REMOTE $ifconfig_pool_remote_ip FROM $untrusted_ip"
 }
 
 # /sbin/arp -i eth0 -Ds $ifconfig_pool_remote_ip eth0 pub
 
-#mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
+mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
--- my_scripts/client_disconnect.sh
+++ my_scripts/client_disconnect.sh
@@ -3,11 +3,11 @@
 function mail_input() {
   echo "To: $1"
   echo "From: ars-spamer@yandex.ru"
-  echo "Subject: ovpn-down home"
+  echo "Subject: ovpn-down VPS"
   echo ""
   echo "DOWN $common_name OVPN $ifconfig_local REMOTE $ifconfig_pool_remote_ip FROM $untrusted_ip"
 }
 
 # /sbin/arp -i eth0 -d $ifconfig_pool_remote_ip
 
-#mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
+mail_input ars.anosov@gmail.com | /usr/bin/msmtp --file=/etc/openvpn/msmtp.conf ars.anosov@gmail.com
